998bd59e82696f7fba230c61d2548204e3de42d8
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-08-26 11:02:34 -0700

    for all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	 char doo[55] = ""Programming is like building a multilingual puzzle";
+	puts(doo);
+	return (0)
+}
--- 0x00-hello_world/4.puts.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-main()
-{
-	 char str[55]= ""Programming is like building a multilingual puzzle";
-	puts(str);
-	return (0)
-}
