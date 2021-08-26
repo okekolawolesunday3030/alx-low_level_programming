4826968fab8b347705a7dc7ef78b23bdabf04c8d
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-08-26 10:45:56 -0700

    all

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+			 
+		 printf("\nSize of a char: %d",sizeof(char));
+		   printf("\nSize of a int: %d",sizeof(int));
+		printf("\nSize of a long int: %d",sizeof(long int));
+		  printf("\nSize of a long long: %d",sizeof(long long));
+		printf("\nSize of a float: %d",sizeof (float));   
+	 return 0;
+}
+
