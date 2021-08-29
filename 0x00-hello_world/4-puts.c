95a80b78e2e31e3f2a19c5343da20a93cc67ede5
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-08-28 17:32:35 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-/* 
- * main: Entry point
- * Description: prints a sample string
- * Return: 0 if program executes properly, else a non-zero number
- */
+/**
+*main: Entry point
+*Description: prints a sample string
+*Return: 0 if program executes properly, else a non-zero number
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
