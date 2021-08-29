6665e0c475aec635708c1d6a006dfdd7f2ffeba3
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-08-28 17:53:42 -0700

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-printf("\nSize of a char: %d byte(s)", sizeof(char));
+printf("Size of a char: %d byte(s)", sizeof(char));
 printf("\nSize of a int: %d byte(s)", sizeof(int));
 printf("\nSize of a long int: %d byte(s)", sizeof(long int));
 printf("\nSize of a long long: %d byte(s)", sizeof(long long));
