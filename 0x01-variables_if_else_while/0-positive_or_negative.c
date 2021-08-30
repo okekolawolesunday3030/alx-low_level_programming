083d5ac156dc462b1d47c4560ce469190e520c48
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-08-29 17:26:10 -0700

    p and n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%i is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%i is zero\n");
+		printf("%d is zero\n", n);
 	}
 	else if (n > 0)
 	{
-		printf("%i is negative\n");
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
