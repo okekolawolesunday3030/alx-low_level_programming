b2067896978b7e042894598cde86db60aa9835c8
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-01 03:58:15 -0700

    changes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
  */
 void print_to_98(int n)
 {
-	int num, ult, prod;
+	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -20,11 +20,14 @@ void print_to_98(int n)
 				_putchar(' ');
 
 				prod = num * mult;
+
 				if (prod <= 99)
 					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
-				{
+				
+				if (prod >= 100)
+				{				{
 					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
