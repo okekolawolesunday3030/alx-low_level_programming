735c1adf124fec50fc65473991ff1b61cc85c76d
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-08-31 17:30:11 -0700

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,14 +9,13 @@
  */
 int main(void)
 {
-  char c[8] = ["Holberton"];
-    
-  unsigned int i;
+	char c[8] = "Holberton";
+	unsigned int i;
 
-    for (i = 0; i < sizeof (c); i++)
-      {
-	_putchar(c[i]);
-      }
-    _putchar('\n');
-    return (0);
+	for (i = 0; i < sizeof(c); i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
