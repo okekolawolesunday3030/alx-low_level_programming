b559fa488a55b7c75b9e352606f2d6a4a845b8bf
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-02 07:32:57 -0700

    _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * -isupper.c - function that checks for uppercase character
+ *  @c: parameter
+ *  Return: 1 if c is uppercase or otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c = 'a' && c <= 'z')
+	{
+		_putchar(1);
+	}
+	else
+	{
+		_putchar(0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+#more functions nested loops
+
+int _isupper(int c) - function that checks for uppercase character
