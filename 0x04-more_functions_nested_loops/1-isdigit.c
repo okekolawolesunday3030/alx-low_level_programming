7d4a0c33e33f366639a18e5785bd0e907ab4f3f4
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-03 07:40:04 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-/** 
+/**
  * _isdigit - function that checks for a digit (0 and 9)
  * @c: int is used as data type
  * Return: 1 or otherwise
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,6 +8,8 @@ void more_numbers(void)
 	{
 	for (c = 0; c <= 14; c++)
 	{
+		if (c > 9)
+			_putchar((num / 10) + '0');
 		_putchar((c % 10) + '0');
 	}
 	_putchar('\n');
