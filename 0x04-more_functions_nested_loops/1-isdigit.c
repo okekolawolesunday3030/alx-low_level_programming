70c1d48bc87fdb1331669fc7442d9030e3951a30
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-03 06:14:14 -0700

    _isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,16 +4,12 @@
 /** 
  * _isdigit - function that checks for a digit (0 and 9)
  * @c: int is used as data type
- * Return: 0
+ * Return: 1 or otherwise
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-	{
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
