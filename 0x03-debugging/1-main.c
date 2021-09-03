1ce3ebe9f8719f9714cf45ba1eb6fb967c0f896e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-03 08:02:37 -0700

    we

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,15 +8,14 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming:\n");
-	i = 0;
-	/*
-	 * while (i < 0)
-	 *
-	 * //{
-	 *      putchar(i);
-	 * //}
-	 */
-	printf("Infinite loop avoided!\o\n");
+	printf("Infinite loop incoming :(\n");
+
+	/*i = 0;*/
+	/* while (i < 0)*/
+	 
+	/*{*/
+	 /*  putchar(i); */
+	 /* //}*/
+       	printf("Infinite loop avoided! \\o\n");
 	return (0);
 }
