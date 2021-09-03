c21388edc8adf3d972298aedd1a9dc49d417748a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-03 08:08:34 -0700

    we

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 #include <time.h>
-#include "HOLBERTON.H"
-/**
- * main - print if the number is postive, zero, or negative
+
+/** main - print if the number is postive, zero, or negative
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
  * Return: 0.
