c8757d2814e1214f1ea0325f9dc45067ed7c5e3f
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-11 14:26:09 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _strncpy - Copies at most an inputted number
  *            of bytes from string src into dest.
  * @dest: The buffer storing the string copy.
@@ -14,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0, src_len = 0;
 
-	while (src[index]++)
+	while (src[index++])
 		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
