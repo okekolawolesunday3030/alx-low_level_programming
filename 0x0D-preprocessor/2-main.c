b9b9ca4012a6c01593f2de10635b09451a555799
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-27 13:10:14 -0700

    prepro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main: program that prints the name of the file 
+ * it was compiled from, followed by a new line.
+ */
+int main(void)
+{
+	printf("file :%a\n", _FILE_);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro_h
+ * Desc: ABS(X) that computes absolute value of x
+ */
+#define ABS(X) ((x) < 0 ? (-x) : (x))
+
+#endif
