819877784fc78831842e6d455ff7036324ab46ed
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-27 13:14:41 -0700

    prepro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,7 +2,7 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /**
- * File: 3-function_like_macro_h
+ * File: 3-function_like_macro.h
  * Desc: ABS(X) that computes absolute value of x
  */
 #define ABS(X) ((x) < 0 ? (-x) : (x))
