656e08d4e30110206b34327e781b96edeadf7081
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-27 12:22:32 -0700

    oject_like_macro

---
+++ 0x0d-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro_h
+ * Desc: Header file that defines macro name
+ * 	SIZE as abbr for token 1024
+ */
+#define SIZE 1024
+#endif
