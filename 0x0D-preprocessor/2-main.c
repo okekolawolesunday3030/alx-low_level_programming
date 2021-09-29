a6bab3d43e6302afd40d233cf2152795cb98e2c2
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2021-09-29 13:11:37 -0700

    we

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/*
- * main - program that prints the name of the file 
- * it was compiled from, followed by a new line.
+/**
+ * main - program that prints the name of the file
+ * Desc: it was compiled from, followed by a new line.
  * Return: 0
  */
 int main(void)
