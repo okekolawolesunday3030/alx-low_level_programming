66539f77a50bc377ff558e82e3153b16bb71febe
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-16 02:55:15 -0800

    line count

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,6 +9,7 @@ int main(void)
 /*int i;*/
 
 printf("Infinite loop incoming :(\n");
+
 /*i = 0;
  *
  * while (i < 0)
@@ -17,5 +18,7 @@ printf("Infinite loop incoming :(\n");
  * }
  */
 printf("Infinite loop avoided! \\o\n");
+
 return (0);
+
 }
