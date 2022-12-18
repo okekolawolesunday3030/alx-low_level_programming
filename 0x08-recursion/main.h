567081a37f7cb7951833af0fc50f325c7e217c61
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-18 12:50:52 -0800

    a

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,8 +6,9 @@
  * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x07-recursion directory.
- */
-
+*/
+#include <stdio.h>
+void positive_or_negative(int i);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
