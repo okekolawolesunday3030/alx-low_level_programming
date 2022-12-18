21d0e352467069a3ab44cd73a31013226a6b59b2
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-18 13:51:18 -0800

    a

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,15 +2,17 @@
 #include <string.h>
 
 /**
- * _strlen_recursion - function returns the length of string
+ * _strlen_recursion- function returns the length of string
+ * Return: char type
  * @s: parameter used
  */
 int _strlen_recursion(char *s)
 {
-	int length ;
+	int length = 0;
 
 	if (*s)
-	
-	length = strlen(s);
+	{
+		length = strlen(s);
+	}
 	return (length);
 }
