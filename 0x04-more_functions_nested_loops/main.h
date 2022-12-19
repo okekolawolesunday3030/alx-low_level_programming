07c8fed7712651c969903dc60bbf4cfe9a53860a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-18 23:46:13 -0800

    a

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,6 +16,7 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_more_numbers(void);
 void print_line(int n);
+int _putchar(char c);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
