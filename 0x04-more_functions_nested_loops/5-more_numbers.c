52fa3a3d2055575c23361d860749d20d465a8b9e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-19 00:13:59 -0800

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
-/** more_numbers.c - function that prints 10 times the numbers, from 0 to 14
+/**
+ *  more_numbers- function that prints 10 times the numbers, from 0 to 14
+ *
  */
 void more_numbers(void)
 {
-	for (j = 0; j <= 10; j++)
+	int j, c;
+
+	for (j = 0; j < 10; j++)
 	{
-	for (c = 0; c <= 14; c++)
-	{
-		if (c > 9)
-			_putchar((num / 10) + '0');
-		_putchar((c % 10) + '0');
-	}
-	_putchar('\n');
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
