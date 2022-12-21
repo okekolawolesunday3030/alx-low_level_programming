3c2bca3ae06deb5a26353888a4839b8757b54170
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-12-21 02:42:12 -0800

    gerrit

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -2,26 +2,6 @@
 #include "main.h"
 
 /**
- *_strlen - returns the length of a string
- *@str:a string of length to be returned
- *Return: returns the length of a string
- */
-int _strlen(char *str)
-{
-	int length = 0;
-
-	while (*str)
-	{
-		str++;
-		length++;
-	}
-
-	return (length);
-
-}
-
-
-/**
  *_strcat - concatinates two strings
  *@dest:destination pointer
  *@src:pointer to a string
